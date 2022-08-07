ac0f52d3b21694bd4ec7b229e9920e0f296e2e28
SudhanshuYadav1998 <sy7040@gmail.com>
2022-08-08 01:34:31 +0530

    UC-3 Exeception file Added

---
+++ Day-20-UC3-Exception/Day-20-UC3-Exception/moodanalyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20_UC3_Exception
+{
+    public class MoodAnalyserException : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            NULL_EXCEPTION, EMPTY_EXCEPTION
+        }
+        public MoodAnalyserException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
